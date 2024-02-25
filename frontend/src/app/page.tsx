5a7a6ee37af932a63594344ce3ca0df43957da9f
Divy Vinayak Diwedi <divyvinayak@gmail.com>
2024-02-25 15:51:17 +0530

    server component for data fetching

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,12 @@
-"use client";
-
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [val, setVal] = useState("");
-  useEffect(() => {
-    fetch('http://localhost:8000/').then(val => val.json()).then(data => setVal(data['Hello']))
-  }, []);
-  return (
-    <>
-      {val}
-    </>
-  );
+async function getData() {
+  const data = await fetch(`${process.env.BACKEND_URL}/`).then(val => val.json());
+  return data.Hello;
+}
+
+export default async function Home() {
+    const val = await getData();
+    return <>{val}</>;
 }
