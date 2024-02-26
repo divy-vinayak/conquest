0c69bd437c6230659e5a92ebc9c5b30f0e2658d2
Divy Vinayak Diwedi <divyvinayak@gmail.com>
2024-02-26 09:18:57 +0530

    css for dark backgroud and NavBar structure made

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function NavBar() {
-  return (
-    <div>
-      This is Nav Bar
-    </div>
-  )
+    return (
+        <div className="flex justify-around p-5">
+            <div>
+                <Link href={"/"}>ConQuest</Link>
+            </div>
+            <div className="flex gap-2">
+                <Link href={"/home"}>Home</Link>
+                <Link href={"/about"}>About</Link>
+            </div>
+            <div className="flex gap-2">
+                <Link href={"/login"}>
+                    <button>Login</button>
+                </Link>
+                <Link href={"/signup"}>
+                    <button>Get Started</button>
+                </Link>
+            </div>
+        </div>
+    );
 }
