cedaa8b4176c59c82863be92c4ab1cc6cca5d5d7
Divy Vinayak Diwedi <divyvinayak@gmail.com>
2024-02-29 14:26:36 +0530

    update

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Response, status
+from fastapi.security import HTTPBearer
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+token_auth_scheme = HTTPBearer()
 
 origins = [
     'http://localhost:3000'
@@ -17,6 +19,6 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-@app.get("/")
-async def read_root():
+@app.get("/api/")
+async def read_root(token: str = Depends(token_auth_scheme)):
     return {"Hello": "World"}
