6dc80a6c39610798d010619c1a76444f01eedaf8
Divy Vinayak Diwedi <divyvinayak@gmail.com>
2024-02-29 22:32:44 +0530

    dashboard

--- frontend/src/app/(main)/dashboard/page.tsx
+++ frontend/src/app/(main)/dashboard/page.tsx
@@ -1,16 +1,15 @@
-import { getSession } from "@auth0/nextjs-auth0";
-
 export default async function Page() {
-    const session = await getSession();
-    const user = session?.user;
-    console.log({user: user})
     return (
         <div>
-            {
-                user? <h1 className="text-4xl justify-center font-bold  text-teal-400 mt-10">Welcome back, {user.given_name} </h1>: null 
-            }
-        
-        <h1 className="text-xl justify-center font-light mb-8 text-grey">Lorem Ipsum is simply dummy text of the printing.</h1>
+            {true ? (
+                <h1 className="text-4xl justify-center font-bold  text-teal-400 mt-10">
+                    Welcome back, 
+                </h1>
+            ) : null}
+
+            <h1 className="text-xl justify-center font-light mb-8 text-grey">
+                Lorem Ipsum is simply dummy text of the printing.
+            </h1>
         </div>
-      );
-}   
+    );
+}
